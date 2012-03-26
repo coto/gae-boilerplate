9b7a4ce8ee2de01536192ac813d7b9a81d55d6bd
Coto <rodrigo.augosto@gmail.com>
2012-03-26 10:31:20 -0300

    move basehanldler within of lib folder

---
+++ lib/basehandler.py
@@ -0,0 +1,105 @@
+
+import webapp2
+from webapp2_extras import jinja2
+from webapp2_extras import auth
+from webapp2_extras import sessions
+
+
+def user_required(handler):
+    """
+         Decorator for checking if there's a user associated
+         with the current session.
+         Will also fail if there's no session present.
+    """
+
+    def check_login(self, *args, **kwargs):
+        """
+            If handler has no login_url specified invoke a 403 error
+        """
+        auth = self.auth.get_user_by_session()
+        if not auth:
+            try:
+                self.redirect(self.auth_config['login_url'], abort=True)
+            except (AttributeError, KeyError), e:
+                self.abort(403)
+        else:
+            return handler(self, *args, **kwargs)
+
+    return check_login
+
+
+class BaseHandler(webapp2.RequestHandler):
+    """
+        BaseHandler for all requests
+
+        Holds the auth and session properties so they
+        are reachable for all requests
+    """
+
+    def dispatch(self):
+        """
+            Get a session store for this request.
+        """
+        self.session_store = sessions.get_store(request=self.request)
+
+        try:
+            # Dispatch the request.
+            webapp2.RequestHandler.dispatch(self)
+        finally:
+            # Save all sessions.
+            self.session_store.save_sessions(self.response)
+
+    @webapp2.cached_property
+    def auth(self):
+        return auth.get_auth()
+
+    @webapp2.cached_property
+    def session_store(self):
+        return sessions.get_store(request=self.request)
+
+    @webapp2.cached_property
+    def session(self):
+        # Returns a session using the default cookie key.
+        return self.session_store.get_session()
+
+    @webapp2.cached_property
+    def messages(self):
+        return self.session.get_flashes(key='_messages')
+
+    def add_message(self, message, level=None):
+        self.session.add_flash(message, level, key='_messages')
+
+    @webapp2.cached_property
+    def auth_config(self):
+        """
+              Dict to hold urls for login/logout
+        """
+        return {
+            'login_url': self.uri_for('login'),
+            'logout_url': self.uri_for('logout')
+        }
+
+    @webapp2.cached_property
+    def user(self):
+        return self.auth.get_user_by_session()
+
+    @webapp2.cached_property
+    def user_id(self):
+        return str(self.user['user_id']) if self.user else None
+
+    @webapp2.cached_property
+    def jinja2(self):
+        return jinja2.get_jinja2(app=self.app)
+
+    def render_template(self, filename, **kwargs):
+        kwargs.update({
+            'current_user': self.user,
+            'current_url': self.request.url,
+            })
+        kwargs.update(self.auth_config)
+        if self.messages:
+            kwargs['messages'] = self.messages[0][0]
+            kwargs['level'] = self.messages[0][1]
+
+        self.response.headers.add_header('X-UA-Compatible', 'IE=Edge,chrome=1')
+        self.response.write(self.jinja2.render_template(filename, **kwargs))
