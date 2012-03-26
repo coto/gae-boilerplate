0d5b5880cf470bb8cdaca08bbe7151abd54bc264
Coto <rodrigo.augosto@gmail.com>
2012-03-26 04:20:23 -0300

    improve code following Google Python Style Guide (http://google-styleguide.googlecode.com/svn/trunk/pyguide.html)

--- lib/functions.py
+++ lib/functions.py
@@ -1,12 +1,18 @@
 
-import os, re, hashlib, Cookie
-from datetime import datetime, timedelta
+import os
+import re
+import hashlib
+import Cookie
+from datetime import datetime
+from datetime import timedelta
 
 # If you move these methods into your own classes, then rename fake_fake_self
 # to fake_self.
 
+
 def encrypt(plaintext, salt="", sha="512"):
     """ Returns the encrypted hexdigest of a plaintext and salt"""
+
     if sha == "1":
         phrase = hashlib.sha1()
     elif sha == "256":
@@ -15,6 +21,7 @@ def encrypt(plaintext, salt="", sha="512"):
         phrase = hashlib.sha512()
     phrase.update("%s@%s" % (plaintext, salt))
     return phrase.hexdigest()
+
 
 def write_cookie(fake_self, COOKIE_NAME, COOKIE_VALUE, path, expires=7200):
     """
@@ -25,17 +32,21 @@ def write_cookie(fake_self, COOKIE_NAME, COOKIE_VALUE, path, expires=7200):
     expires = 1209600 # 2 weeks
     expires = 2629743 # 1 month
     """
-    time_expire = datetime.now() + timedelta(seconds=expires) # days, seconds, then other fields.
-    time_expire = time_expire.strftime("%a, %d-%b-%Y %H:%M:%S GMT")
-    #    print time_expire
 
-    fake_self.response.headers.add_header('Set-Cookie', COOKIE_NAME+'='+COOKIE_VALUE+'; expires='+str(time_expire)+'; path='+path+'; HttpOnly')
+    # days, seconds, then other fields.
+    time_expire = datetime.now() + timedelta(seconds=expires)
+    time_expire = time_expire.strftime("%a, %d-%b-%Y %H:%M:%S GMT")
+
+    fake_self.response.headers.add_header(
+        'Set-Cookie', COOKIE_NAME+'='+COOKIE_VALUE+'; expires='+str(time_expire)+'; path='+path+'; HttpOnly')
     return
+
 
 def read_cookie(fake_self, name):
     """
     Use: cook.read(fake_self, COOKIE_NAME)
     """
+
     string_cookie = os.environ.get('HTTP_COOKIE', '')
     fake_self.cookie = Cookie.SimpleCookie()
     fake_self.cookie.load(string_cookie)
@@ -44,13 +55,13 @@ def read_cookie(fake_self, name):
         value  = fake_self.cookie[name].value
 
     return value
-    # Old Way
-#    value = fake_self.request.cookies.get(name)
+
 
 def get_date_time(format="%Y-%m-%d %H:%M:%S", UTC_OFFSET=3):
     """
     Get date and time in UTC for Chile with a specific format
     """
+
     local_datetime = datetime.now()
     now = local_datetime - timedelta(hours=UTC_OFFSET)
     if format != "datetimeProperty":
@@ -58,16 +69,19 @@ def get_date_time(format="%Y-%m-%d %H:%M:%S", UTC_OFFSET=3):
     #    now = datetime.fromtimestamp(1321925140.78)
     return now
 
+
 def is_email_valid(email):
     if len(email) > 7:
         if re.match("^.+\\@(\\[?)[a-zA-Z0-9\\-\\.]+\\.([a-zA-Z]{2,3}|[0-9]{1,3})(\\]?)$", email) != None:
             return 1
     return 0
 
+
 def is_alphanumeric(field):
     if re.match("^\w+$", field) is not None:
         return 1
     return 0
+
 
 def get_device(fake_self):
     uastring = fake_self.request.user_agent
