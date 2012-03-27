7523ad37fbd569aa6803db77e64ac897a15a7224
Coto <rodrigo.augosto@gmail.com>
2012-03-26 21:34:00 -0300

    get device to define desktop or mobile version and add behavior for Language select and device button

--- static/js/script.js
+++ static/js/script.js
@@ -211,10 +211,50 @@ var MD5 = function (string) {
     return temp.toLowerCase();
 }
 
+function insertParam(key, value)
+{
+    key = escape(key); value = escape(value);
+
+    var kvp = document.location.search.substr(1).split('&');
+
+    var i=kvp.length; var x;
+    while(i--) {
+        x = kvp[i].split('=');
+
+        if (x[0]==key)
+        {
+            x[1] = value;
+            kvp[i] = x.join('=');
+            break;
+        }
+    }
+
+    if(i<0) {kvp[kvp.length] = [key,value].join('=');}
+
+    //this will reload the page, it's likely better to store this until finished
+    document.location.search = kvp.join('&');
+}
+
 
 $(document).ready(function() {
 
     $("body").css({"visibility": "visible"});
+
+    /***** Choose language @ footer ****/
+    $("#select_language").change(function() {
+        newLang = $(this).val();
+        if ($(this).attr("current") != newLang) {
+            insertParam("hl", newLang)
+        }
+    });
+
+    /***** Choose language @ footer ****/
+    $(".select_device").click(function() {
+        newDev = $(this).attr("data-device");
+        if ($(this).attr("current") != newDev) {
+            insertParam("device", newDev)
+        }
+    });
 });
 
 
