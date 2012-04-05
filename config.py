f262c6cc94374a64ea77d8dbe52c97d8ef1d0dc6
Pieter Retief <peterretief@gmail.com>
2012-04-05 17:29:35 +0200

    Moved models.py to models dir/

--- config.py
+++ config.py
@@ -5,6 +5,6 @@ webapp2_config['webapp2_extras.sessions'] = {
     'secret_key': '_PUT_KEY_HERE_YOUR_SECRET_KEY_',
     }
 webapp2_config['webapp2_extras.auth'] = {
-    'user_model': 'models.User',
+    'user_model': 'models.models.User',
     'cookie_name': 'session_name'
 }
