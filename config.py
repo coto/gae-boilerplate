b025b088b2e33fde4479eca4227dc32d1f79790e
Pedro Manacas <pedromanacas@gmail.com>
2012-05-07 00:42:08 -0400

    user author

--- config.py
+++ config.py
@@ -8,3 +8,10 @@ webapp2_config['webapp2_extras.auth'] = {
     'user_model': 'models.models.User',
     'cookie_name': 'session_name'
 }
+
+# get your own recaptcha keys by registering at www.google.com/recaptcha
+captcha_public_key = "PUT_YOUR_RECAPCHA_PUBLIC_KEY_HERE"
+captcha_private_key = "PUT_YOUR_RECAPCHA_PRIVATE_KEY_HERE"
+
+google_analytics_code = "UA-XXXXX-X"
+
