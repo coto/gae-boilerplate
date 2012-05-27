05089337bcecbcbb28a0e1f5fb0ccef23658ae50
Coto <rodrigo.augosto@gmail.com>
2012-05-27 04:54:55 -0400

    landing page #19

--- config.py
+++ config.py
@@ -9,13 +9,12 @@ webapp2_config['webapp2_extras.auth'] = {
     'cookie_name': 'session_name'
 }
 
-contact_recipient = "PUT_YOUR_EMAIL_HERE"
+contact_recipient = "rodrigo.augosto@gmail.com"
 
 salt = "_PUT_SALT_HERE_TO_SHA512_PASSWORDS_"
 
 # get your own recaptcha keys by registering at www.google.com/recaptcha
-captcha_public_key = "PUT_YOUR_RECAPCHA_PUBLIC_KEY_HERE"
-captcha_private_key = "PUT_YOUR_RECAPCHA_PRIVATE_KEY_HERE"
+captcha_public_key = "6LfUI9ESAAAAAGYPG443J5pddULoYMCG0vLHa47_"
+captcha_private_key = "6LfUI9ESAAAAAIgNTwGtPrRaCY-VqijrBVR85Zdj"
 
-google_analytics_code = "UA-XXXXX-X"
-
+google_analytics_code = "UA-15733015-4"
