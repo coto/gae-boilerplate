2ccdeb5420813f3a18f36c52c8a814d59fe4d867
peta15 <jessegoodall@gmail.com>
2012-06-21 01:21:29 -0400

    webapp2 i18n implementation.  i18n language detection rewritten. now uses locales rather than raw languages

--- config.py
+++ config.py
@@ -4,10 +4,14 @@ boilerplate_version = "Google App Engine Boilerplate 2.0 RC1"
 webapp2_config = {}
 webapp2_config['webapp2_extras.sessions'] = {
     'secret_key': '_PUT_KEY_HERE_YOUR_SECRET_KEY_',
-    }
+}
 webapp2_config['webapp2_extras.auth'] = {
     'user_model': 'models.models.User',
     'cookie_name': 'session_name'
+}
+webapp2_config['webapp2_extras.jinja2'] = {
+    'template_path': 'templates',
+    'environment_args': {'extensions': ['jinja2.ext.i18n']},
 }
 
 contact_recipient = "PUT_YOUR_EMAIL_HERE"
--- lib/i18n.py
+++ lib/i18n.py
@@ -1,134 +1,142 @@
-
-__author__ = 'Rodrigo Augosto (@coto)'
-__website__ = 'www.beecoss.com'
-
-import operator
 import re
 import logging
 from lib import utils
-from lib import languages
 from google.appengine.api.urlfetch_errors import DownloadError
 from google.appengine.api import urlfetch
+from webapp2_extras import i18n
 
+# Locale code = <language>_<territory> (ie 'en_US')
+# Language codes defined under iso 639-1 http://en.wikipedia.org/wiki/List_of_ISO_639-1_codes
+# Territory codes defined under iso 3166-1 alpha-2 http://en.wikipedia.org/wiki/ISO_3166-1
+# Available locales should be in descending priority order.  ie the first entry is default and if 
+# there are for example en_US followed by en_GB then en_US will take priority 
+# if the language detected is english byt territory could not be detected
+AVAILABLE_LOCALES = ['en_US', 'es_ES', 'it_IT', 'zh_CN']
+LANGUAGES = {
+             'en_US': 'English',
+             'es_ES': 'Spanish',
+             'it_IT': 'Italian',
+             'zh_CN': 'Chinese'
+             }
 
-def __findall(pattern, string):
+def parse_accept_language_header(string, pattern='([a-zA-Z]{1,8}(-[a-zA-Z0-9]{1,8})?)\s*(;\s*q\s*=\s*(1|0\.[0-9]+))?'):
     """
-    Break up string into pieces (languages and q factors)
+    Parse a dict from an Accept-Language header string
+    (see http://www.w3.org/Protocols/rfc2616/rfc2616-sec14.html)
+    example input: en-US,en;q=0.8,es-es;q=0.5
+    example output: {'en_US': 100, 'en': 80, 'es_ES': 50}
     """
     res = {}
+    if not string: return None
     for match in re.finditer(pattern, string):
         if None == match.group(4):
-            key = 1
+            q = 1
         else:
-            key = match.group(4)
-        res[match.group(1)] = int(100*float(key))
+            q = match.group(4)
+        l = match.group(1).replace('-','_')
+        if len(l) == 2:
+            l = l.lower()
+        elif len(l) == 5:
+            l = l.split('_')[0].lower() + "_" + l.split('_')[1].upper()
+        else:
+            l = None
+        if l:
+            res[l] = int(100*float(q))
     return res
 
-def get_country(cls):
+def get_territory_from_ip(cls):
     """
-    Detect th country origin with the IP
-    Returns us, ca, cl, ar, etc.
-    You can get the flag with http://geoip.wtanaka.com/flag/us.gif
+    Detect the territory code derived from IP Address location
+    Returns US, CA, CL, AR, etc.
+    cls: self object
+    
+    Uses lookup service http://geoip.wtanaka.com/cc/<ip>
+    You can get a flag image given the returned territory 
+        with http://geoip.wtanaka.com/flag/<territory>.gif
+        example: http://geoip.wtanaka.com/flag/us.gif
     """
-    country = "CL"
+    territory = None
     try:
         result = urlfetch.fetch("http://geoip.wtanaka.com/cc/"+cls.request.remote_addr)
         if result.status_code == 200:
             fetch = result.content
             if len(str(fetch)) < 3:
-                country = str(fetch).upper()
+                territory = str(fetch).upper()
             else:
                 logging.warning("Ups, geoip.wtanaka.com is not working. Look what it returns: "+ str(fetch) )
         else:
             logging.warning("Ups, geoip.wtanaka.com is not working. Status Code: "+ str(result.status_code) )
     except DownloadError:
         logging.warning("Couldn't resolve http://geoip.wtanaka.com/cc/"+cls.request.remote_addr)
-    return country
+    return territory
 
-
-def get_languages(cls, type="array"):
+def get_locale_from_territory(territory):
     """
-    Detect languages from request.header (accept-language)
-    or from url fetch service if the first one does not works
+    Returns locale from AVAILABLE_LOCALES given a territory code
+    """
+    available_territories = [locale.split('_')[1] for locale in AVAILABLE_LOCALES]
+    for i,t in enumerate(available_territories):
+        if t == territory:
+            return AVAILABLE_LOCALES[i]
 
+def get_locale_from_accept_header(cls):
+    """
+    Detect locale from request.header 'Accept-Language'
+    Locale with the highest quality factor that most nearly matches our 
+    AVAILABLE_LOCALES is returned.
     cls: self object
 
-    type: (optional) can be "array", "tuple" or "original"
-    and defines how to return the information
+    Note that in the future if
+        all User Agents adopt the convention of sorting quality factors in descending order
+        then the first can be taken without needing to parse or sort the accept header
+        leading to increased performance
+        (see http://lists.w3.org/Archives/Public/ietf-http-wg/2012AprJun/0473.html)
     """
-    accept_language = None
-    arrLang = []
+    available_languages = [locale.split('_')[0] for locale in AVAILABLE_LOCALES]
+    header = cls.request.headers.get("Accept-Language", '')
+    parsed = parse_accept_language_header(header)
+    for item in sorted(parsed.iteritems()):
+        if item[0] in AVAILABLE_LOCALES:
+            return item[0]
+        for i,l in enumerate(available_languages):
+            if item[0] == l:
+                return AVAILABLE_LOCALES[i]
 
-    try:
-        accept_language = cls.request.headers["Accept-Language"]
-    except NameError:
-        logging.warning("i18n | Nameerror for accept_language")
-        accept_language = None
-    except AttributeError:
-        logging.warning("i18n | AttributeError for accept_language")
-        accept_language = None
-    except KeyError:
-        logging.warning("i18n | KeyError for accept_language")
-        accept_language = None
-
-    if accept_language is None:
-        # Get language with url fetch based on IP
-        tupleLang = get_country(cls)
-        accept_language = tupleLang
-        arrLang.append(tupleLang)
-    else:
-        tupleLang = __findall('([a-zA-Z]{1,8}(-[a-zA-Z0-9]{1,8})?)\s*(;\s*q\s*=\s*(1|0\.[0-9]+))?', accept_language)
-
-        # sort list based on value
-        tupleLang = sorted(tupleLang.iteritems(), key=operator.itemgetter(1), reverse=True)
-
-        # look through sorted list and use first one that matches our languages
-        for lang in tupleLang:
-            arrLang.append(lang[0])
-    if type == "array":
-        giveValue = arrLang
-    elif type == "tuple":
-        giveValue = tupleLang
-    elif type == "original":
-        giveValue = accept_language
-    else:
-        giveValue = arrLang
-    return giveValue
-
-
-def set_lang_cookie_and_return_dict(cls, force=""):
+def set_locale(cls, force=None):
     """
-    set language returning a dict and set cookie
+    retrieve locale from a prioritized list of sources and then set locale and save it
+    cls: self object
+    force: a locale to force set (ie 'en_US')
+    return: locale
     """
-    if force != "":
-        lang_cookie = force
-    elif cls.request.get("hl") == "":
-        # ask for cookie
-        lang_cookie = utils.read_cookie(cls, "hl")
-        arrLang = str(get_languages(cls)[0]).lower()
-        if not lang_cookie:
-            if arrLang.find("es") >= 0:
-                lang_cookie = "es"
-            else:
-                lang_cookie = "en"
-    else:
-        # set cookie to param 'hl' value
-        lang_cookie = cls.request.get("hl")
-        # Two weeks for hl cookie
-    utils.write_cookie(cls, "hl", str(lang_cookie), "/", 1209600)
+    # 1. force locale if provided
+    locale = force
+    if locale not in AVAILABLE_LOCALES:
+        # 2. retrieve locale from url query string
+        locale = cls.request.get("hl", None)
+        if locale not in AVAILABLE_LOCALES:
+            # 3. retrieve locale from cookie
+            locale = utils.read_cookie(cls, "hl")
+            if locale not in AVAILABLE_LOCALES:
+                # 4. retrieve locale from accept language header
+                locale = get_locale_from_accept_header(cls)
+                if locale not in AVAILABLE_LOCALES:
+                    # 5. detect locale from IP address location
+                    locale = get_locale_from_territory(get_territory_from_ip(cls))
+                    if locale not in AVAILABLE_LOCALES:
+                        # 6. use default locale
+                        locale = AVAILABLE_LOCALES[0]
+    # convert unicode locale to string for headers
+    locale = str(locale)
+    i18n.get_i18n().set_locale(locale)
+    # save locale in cookie with 26 weeks expiration (in seconds)
+    utils.write_cookie(cls, "hl", locale, "/", 15724800)
+    return locale
 
-    try:
-        lang = {
-            'en': languages.en,
-            'es': languages.es,
-            'it': languages.it,
-            'cn': languages.cn,
-        }[lang_cookie]
-    except KeyError:
-        lang = {
-            'en': languages.en,
-            'es': languages.es,
-            'it': languages.it,
-            'cn': languages.cn,
-        }['en']
-    return lang
+def get_language(locale):
+    return LANGUAGES[locale]
+
+def get_territory_code(locale):
+    return locale.split('_')[1]
+
+
