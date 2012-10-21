319e514956374c6d54b940cbdcb59f45605d2c9c
peta15 <jessegoodall@gmail.com>
2012-10-21 12:03:43 -0400

    fixed tests to reflect the new warning message in password reset.  Now all tests should pass.

--- boilerplate/lib/test_helpers.py
+++ boilerplate/lib/test_helpers.py
@@ -47,7 +47,7 @@ class HandlerHelpers():
             self.assertListEqual(form_fields, expect_fields)
         return form
 
-    def submit(self, form, expect_error=False, error_message='', error_field='', success_message=''):
+    def submit(self, form, expect_error=False, error_message='', error_field='', success_message='', warning_message=''):
         """Submit the form"""
         response = form.submit(headers=self.headers)
         if response.status_int == 200:
@@ -75,6 +75,8 @@ class HandlerHelpers():
             self.assert_no_error_message_in_response(response)
             if success_message:
                 self.assert_success_message_in_response(response, message=success_message)
+            if warning_message:
+                self.assert_warning_message_in_response(response, message=warning_message)
         return response
 
     def login_user(self, username, password):
