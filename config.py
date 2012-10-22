d4bea89e8fa51ec4c9609770ea16886f4d9ca55d
peta15 <jessegoodall@gmail.com>
2012-10-22 00:21:59 -0400

    New config.py and tests.py in the root and web modules respectively to make kickstarting a new application easy while keeping the boilerplate default config and tests separate for easy future udpates. Documentation and comments updated.  Root config and templates take precedence over boilerplate config and templates.

---
+++ config.py
@@ -0,0 +1,9 @@
+"""
+This configuration file should contain all custom config settings for the application.
+It takes precedence over the config located in the boilerplate package.
+"""
+config = {
+
+# ----> ADD MORE CONFIGURATION OPTIONS HERE <----
+
+} # end config
