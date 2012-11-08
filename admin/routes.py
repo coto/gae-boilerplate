9d61f676a4752317a00596e62a79a58af2644fa0
Serguei Strigo <serguei.strigo@gmail.com>
2012-11-08 22:27:33 +0100

    Add Geochart

--- admin/routes.py
+++ admin/routes.py
@@ -6,6 +6,7 @@ import users
 _routes = [
     PathPrefixRoute('/admin', [
         RedirectRoute('/logout/', users.Logout, name='admin-logout', strict_slash=True),
+        RedirectRoute('/geochart/', users.Geochart, name='geochart', strict_slash=True),
         RedirectRoute('/users/', users.List, name='user-list', strict_slash=True),
         RedirectRoute('/users/<user_id>/', users.Edit, name='user-edit', strict_slash=True, handler_method='edit')
     ])
