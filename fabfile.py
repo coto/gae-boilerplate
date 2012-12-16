2145fd6ea307b3155c4a361148f89c3b70efd409
Coto <rodrigo.augosto@gmail.com>
2012-12-15 22:02:11 -0300

    Replace Makefile with Fabric... documentation in the code

---
+++ fabfile.py
@@ -0,0 +1,86 @@
+"""
+    REQUIREMENTS:
+        - install pip with distribute (http://packages.python.org/distribute/)
+        - sudo pip install Fabric
+
+"""
+
+from fabric.api import local
+
+def lang(mode="extract"):
+    """
+        REQUIREMENTS:
+
+        - Install before pip with distribute_setup.py (Read the environment setup document)
+        - sudo pip install babel
+        - sudo pip install jinja2
+
+        HOW TO RUN:
+
+            option 1) fab lang
+            option 2) fab lang:compile
+    """
+
+    if mode == "compile":
+        local("pybabel compile -f -d ./locale")
+    else:
+        local("pybabel extract -F ./locale/babel.cfg -o ./locale/messages.pot ./ --sort-output --no-location --omit-header")
+        local("pybabel update -l en_US -d ./locale -i ./locale/messages.pot --previous --ignore-obsolete")
+        local("pybabel update -l es_ES -d ./locale -i ./locale/messages.pot --previous --ignore-obsolete")
+        local("pybabel update -l it_IT -d ./locale -i ./locale/messages.pot --previous --ignore-obsolete")
+        local("pybabel update -l zh_CN -d ./locale -i ./locale/messages.pot --previous --ignore-obsolete")
+        local("pybabel update -l id_ID -d ./locale -i ./locale/messages.pot --previous --ignore-obsolete")
+        local("pybabel update -l fr_FR -d ./locale -i ./locale/messages.pot --previous --ignore-obsolete")
+        local("pybabel update -l de_DE -d ./locale -i ./locale/messages.pot --previous --ignore-obsolete")
+        local("pybabel update -l ru_RU -d ./locale -i ./locale/messages.pot --previous --ignore-obsolete")
+        local("pybabel update -l pt_BR -d ./locale -i ./locale/messages.pot --previous --ignore-obsolete")
+
+def start(mode="normal"):
+    """
+        HOW TO RUN:
+
+            option 1) fab start
+            option 2) fab start:clear
+
+    """
+
+    if mode == "clear":
+        local("dev_appserver.py . -p 8080 -a 0.0.0.0 --clear_datastore")
+    else:
+        local("dev_appserver.py . -p 8080 -a 0.0.0.0")
+
+def deploy(app_id="sandengine", version="2-2"):
+    """
+        app.yaml never has to be version:default
+
+        HOW TO RUN:
+
+            option 1) fab deploy
+            option 2) fab deploy:preview
+            option 3) fab deploy:prod
+            option 4) fab deploy:homo
+
+    """
+
+    local("appcfg.py --no_cookies --oauth2 -A {0} -V {1} update .".format(app_id, version))
+
+def test(os="mac"):
+    """
+        REQUIREMENTS:
+        - install pip with distribute (http://packages.python.org/distribute/)
+        - sudo pip install mock
+        - sudo pip install webtest
+        - sudo pip install pyquery
+
+        HOW TO RUN:
+
+            option 1) fab test
+            option 2) fab test:mac
+            option 3) fab test:linux
+
+    """
+    path = {
+        "mac": "/usr/local/google_appengine",
+       }[os]
+
+    local("python testrunner.py {0} ./".format(path))
