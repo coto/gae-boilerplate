d0c20d9e13bf40efd95f07eb43e7ed713f0ce615
Coto <rodrigo.augosto@gmail.com>
2012-12-15 22:02:33 -0300

    improve code

--- config/__init__.py
+++ config/__init__.py
@@ -1,9 +1,9 @@
-import os
-
 """
 This configuration file loads environment's specific config settings for the application.
 It takes precedence over the config located in the boilerplate package.
 """
+
+import os
 
 if "SERVER_SOFTWARE" in os.environ:
     if os.environ['SERVER_SOFTWARE'].startswith('Dev'):
