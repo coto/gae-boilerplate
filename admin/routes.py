05b2fd4e3fd87400211b34a0f2f7dd3fe2715fb4
Coto <rodrigo.augosto@gmail.com>
2013-06-24 22:30:23 -0400

    improve routes for admin

--- admin/routes.py
+++ admin/routes.py
@@ -4,12 +4,10 @@ import users
 
 
 _routes = [
-    PathPrefixRoute('/admin', [
-        RedirectRoute('/logout/', users.AdminLogoutHandler, name='admin-logout', strict_slash=True),
-        RedirectRoute('/', users.AdminGeoChartHandler, name='geochart', strict_slash=True),
-        RedirectRoute('/users/', users.AdminUserListHandler, name='user-list', strict_slash=True),
-        RedirectRoute('/users/<user_id>/', users.AdminUserEditHandler, name='user-edit', strict_slash=True, handler_method='edit')
-    ])
+    RedirectRoute('/admin/logout/', users.AdminLogoutHandler, name='admin-logout', strict_slash=True),
+    RedirectRoute('/admin/', users.AdminGeoChartHandler, name='geochart', strict_slash=True),
+    RedirectRoute('/admin/users/', users.AdminUserListHandler, name='user-list', strict_slash=True),
+    RedirectRoute('/admin/users/<user_id>/', users.AdminUserEditHandler, name='user-edit', strict_slash=True, handler_method='edit')
 ]
 
 def get_routes():
