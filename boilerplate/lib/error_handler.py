d634c04ccb4068ea4ee76c276e26a2cab4741682
Coto <rodrigo.augosto@gmail.com>
2013-06-27 08:03:14 -0400

    better error handler

--- boilerplate/lib/error_handler.py
+++ boilerplate/lib/error_handler.py
@@ -1,13 +1,15 @@
 # *-* coding: UTF-8 *-*
 
 # standard library imports
-import traceback
-import logging
+import os
 import sys
+import logging
+import traceback
 # related third party imports
 import webapp2
 import httpagentparser
 from webapp2_extras import jinja2
+from google.appengine.api import app_identity
 from google.appengine.api import taskqueue
 # local application/library specific imports
 
@@ -27,12 +29,12 @@ def handle_error(request, response, exception):
 
         lines = traceback.format_exception(exc_type, exc_value, exc_tb)
         ua = httpagentparser.detect(request.user_agent)
-        os = ua.has_key('flavor') and 'flavor' or 'os'
+        _os = ua.has_key('flavor') and 'flavor' or 'os'
 
         if len(ua.keys()) > 0:
-            operating_system = str(ua[os]['name'])
-            if 'version' in ua[os]:
-                operating_system += ' ' + str(ua[os]['version'])
+            operating_system = str(ua[_os]['name'])
+            if 'version' in ua[_os]:
+                operating_system += ' ' + str(ua[_os]['version'])
             if 'dist' in ua:
                 operating_system += ' ' + str(ua['dist'])
             browser = str(ua['browser']['name'])
@@ -42,15 +44,16 @@ def handle_error(request, response, exception):
             browser = "-"
             browser_version = "-"
 
-        message = '<strong>app_name:</strong> ' + request.app.config.get('app_name') + "<br />" + \
-                  '<strong>Referer:</strong> ' + str(request.referer) + "<br />" + \
-                  '<strong>User Agent:</strong> ' + str(request.user_agent) + "<br />" + \
+        message = '<strong>Application ID:</strong> ' + app_identity.get_application_id() + "<br />" + \
+                  '<strong>Application Version:</strong> ' + os.environ['CURRENT_VERSION_ID'] + "<br />" + \
+                  '<hr><strong>User Agent:</strong> ' + str(request.user_agent) + "<br />" + \
                   '<strong>Operating System:</strong> ' + str(operating_system) + "<br />" + \
                   '<strong>Browser:</strong> ' + str(browser) + "<br />" + \
                   '<strong>Browser Version:</strong> ' + str(browser_version) + "<br />" + \
-                  '<strong>Type:</strong> ' + exc_type.__name__ + "<br />" + \
+                  '<hr><strong>Error Type:</strong> ' + exc_type.__name__ + "<br />" + \
                   '<strong>Description:</strong> ' + c['exception'] + "<br />" + \
                   '<strong>URL:</strong> ' + c['url'] + "<br />" + \
+                  '<strong>Referer:</strong> ' + str(request.referer) + "<br />" + \
                   '<strong>Traceback:</strong> <br />' + '<br />'.join(lines)
 
         if c['exception'] is not 'Error saving Email Log in datastore':
