1ca3aa235a5d70d829323c15117841496d87ff37
Coto <rodrigo.augosto@gmail.com>
2013-06-28 22:59:10 -0400

    fix travis error failing

--- config/__init__.py
+++ config/__init__.py
@@ -8,11 +8,8 @@ import os
 if "SERVER_SOFTWARE" in os.environ:
     if os.environ['SERVER_SOFTWARE'].startswith('Dev'):
         from config.localhost import config
-        config.environment = "localhost"
 
     elif os.environ['SERVER_SOFTWARE'].startswith('Google'):
         from config.production import config
-        config.environment = "production"
 else:
     from config.testing import config
-    config.environment = "testing"
