e3b75ed474c3e18e0ab3b7d1178078b2a1831a2a
andrewgreensmith <andrewgreensmith@users.noreply.github.com>
2014-05-13 14:58:08 +0100

    added admin pages for email logs and vist logs

--- bp_admin/routes.py
+++ bp_admin/routes.py
@@ -1,13 +1,23 @@
 from webapp2_extras.routes import RedirectRoute
 import users
 import admin
+import emails
+import visitlogs
+import crontasks
 
 
 _routes = [
     RedirectRoute('/admin/', users.AdminUserGeoChartHandler, name='admin-users-geochart', strict_slash=True),
     RedirectRoute('/admin/users/', users.AdminUserListHandler, name='admin-users-list', strict_slash=True),
     RedirectRoute('/admin/users/<user_id>/', users.AdminUserEditHandler, name='admin-user-edit', strict_slash=True, handler_method='edit'),
+
+    RedirectRoute('/admin/emails/', emails.AdminEmailListHandler, name='admin-emails-list', strict_slash=True),
+    RedirectRoute('/admin/emails/<email_id>/', emails.AdminEmailViewHandler, name='admin-email-view', strict_slash=True),
+
+    RedirectRoute('/admin/visitlogs/', visitlogs.AdminVisitLogsListHandler, name='admin-visitlogs-list', strict_slash=True),
+
     RedirectRoute('/admin/logout/', admin.AdminLogoutHandler, name='admin-logout', strict_slash=True),
+
 ]
 
 def get_routes():
